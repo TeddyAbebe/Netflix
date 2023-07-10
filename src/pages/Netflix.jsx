b2f6f0df4d232276d8d6205637a1fb9ed91455df
Tewodros Abebe <teddyab729@gmail.com>
2023-07-10 22:04:56 +0300

    Update

--- src/pages/Netflix.jsx
+++ src/pages/Netflix.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import backgroundImage from "../assets/home.jpg";
@@ -6,10 +6,17 @@ import MovieLogo from "../assets/homeTitle.webp";
 import { FaPlay } from "react-icons/fa";
 import { AiOutlineInfoCircle } from "react-icons/ai";
 import styled from "styled-components";
+import { useDispatch } from "react-redux";
+import { getGenres } from "../store";
 
 export default function Netflix() {
   const [isScrolled, setIsScrolled] = useState(false);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getGenres());
+  }, []);
 
   window.onscroll = () => {
     setIsScrolled(window.pageYOffset === 0 ? false : true);
