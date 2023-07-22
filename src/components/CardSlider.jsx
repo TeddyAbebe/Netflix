05633cd0d05022b4542eb76d1a62524222d6a14f
Tewodros Abebe <teddyab729@gmail.com>
2023-07-22 23:49:54 +0300

    Update

--- src/components/CardSlider.jsx
+++ src/components/CardSlider.jsx
@@ -5,18 +5,26 @@ import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
 
 export default function CardSlider({ data, title }) {
   const [showControls, setShowControls] = useState(false);
-  const [sliderPosition, setSLiderPosition] = useState(0);
+  const [sliderPosition, setSliderPosition] = useState(0);
   const listRef = useRef();
 
   const handleDirection = (direction) => {
-    let distance = listRef
+    let distance = listRef.current.getBoundingClientRect().x - 70;
+    if (direction === "left" && sliderPosition > 0) {
+      listRef.current.style.transform = `translateX(${230 + distance}px)`;
+      setSliderPosition(sliderPosition + 1);
+    }
+    if (direction === "right" && sliderPosition < 4) {
+      listRef.current.style.transform = `translateX(${-230 + distance}px)`;
+      setSliderPosition(sliderPosition + 1);
+    }
   };
 
   return (
     <Container
       className="flex column"
       onMouseEnter={() => setShowControls(true)}
-      onMouseleave={() => setShowControls(false)}
+      onMouseLeave={() => setShowControls(false)}
     >
       <h1>{title}</h1>
       <div className="wrapper">
