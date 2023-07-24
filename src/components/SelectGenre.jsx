fe77289285f74c031657d48f127fed40281ffda0
Tewodros Abebe <teddyab729@gmail.com>
2023-07-24 22:40:56 +0300

    Update

--- src/components/SelectGenre.jsx
+++ src/components/SelectGenre.jsx
@@ -2,10 +2,18 @@ import React from "react";
 import { styled } from "styled-components";
 import { getGenres } from "../store";
 
-export default function SelectGenre({genres, type}) {
-  return <Select>{getGenres.map(
-
-  )}</Select>;
+export default function SelectGenre({ genres, type }) {
+  return (
+    <Select>
+      {getGenres.map((genre) => {
+        return (
+          <option value={genre.id} key={genre.id}>
+            {genre.name}
+          </option>
+        );
+      })}
+    </Select>
+  );
 }
 
 const Select = styled.select``;
